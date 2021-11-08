f3b956d9b36ce106290af088cf9f228a2ea9679d
Gerard Lakin <gerardlakin@gmail.com>
2021-11-07 22:05:12 -0600

    Added Data Preparation script

---
+++ data_preparation.R
@@ -0,0 +1,11 @@
+df <- read.csv("yob2020.txt", header = FALSE, col.names = c("name", "gender", "count"))
+
+df$frequency <- NA
+df[df$gender == 'M',]$frequency <- unlist(df[df$gender == 'M',]$count/sum(df[df$gender == 'M',]$count))
+df[df$gender == 'F',]$frequency <- unlist(df[df$gender == 'F',]$count/sum(df[df$gender == 'F',]$count))
+
+df$rank <- NA
+df[df$gender == 'M',]$rank <- unlist(rank(-df[df$gender == 'M',]$count))
+df[df$gender == 'F',]$rank <- unlist(rank(-df[df$gender == 'F',]$count))
+
+write.csv(df, "baby_name_app/name_data.csv")
--- suggestion_function.R
+++ suggestion_function.R
@@ -1,14 +1,3 @@
-df <- read.csv("yob2020.txt", header = FALSE, col.names = c("name", "gender", "count"))
-
-genders <- c('F','M')
-
-df$frequency <- NA
-df[df$gender == 'M',]$frequency <- unlist(df[df$gender == 'M',]$count/sum(df[df$gender == 'M',]$count))
-df[df$gender == 'F',]$frequency <- unlist(df[df$gender == 'F',]$count/sum(df[df$gender == 'F',]$count))
-
-df$rank <- NA
-df[df$gender == 'M',]$rank <- unlist(rank(-df[df$gender == 'M',]$count))
-df[df$gender == 'F',]$rank <- unlist(rank(-df[df$gender == 'F',]$count))
 
 suggest_name <- function(df, gnd) {
   df_gender <- df[df$gender == gnd,]
@@ -17,4 +6,3 @@ suggest_name <- function(df, gnd) {
 } 
 
 
-write.csv(df, "name_data.csv")
