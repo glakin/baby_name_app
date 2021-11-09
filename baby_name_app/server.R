e35a4a30f8bee13f015e8a09c6826aff1173e595
Gerard Lakin <gerardlakin@gmail.com>
2021-11-08 22:15:59 -0600

    Reorganized UI. Added ability to toggle repeating names on and off. Added section listing how many distinct names the user has seen and what percent of total children they represent.

--- baby_name_app/server.R
+++ baby_name_app/server.R
@@ -1,43 +1,92 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
 df <- read.csv("name_data.csv")
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     values <- reactiveValues()
     
     observeEvent(input$suggest, {
-        df_gender <- df[df$gender == input$gender,]
+        
+        
+        if (input$rpt == "yes" | !file.exists(paste0("observed_names_",input$gender,".csv"))) {
+            df_gender <- df[df$gender == input$gender,]
+        } else {
+            observed_names <- read.csv(paste0("observed_names_",input$gender,".csv"))
+            df_gender <- df[df$gender == input$gender & !(df$name %in% observed_names$name),]
+        }
+        
         values$nameSuggestion <- sample(df_gender$name, size=1, prob=df_gender$frequency )
         values$nameRank <- round(df_gender[df_gender$name == values$nameSuggestion,]$rank)
+        values$nameCount <- df_gender[df_gender$name == values$nameSuggestion,]$count
         output$nameSuggestion <- renderText({
             values$nameSuggestion
         })
         output$nameRank <- renderText({
             paste0("Rank: ", values$nameRank)
         })
+        
+        if(file.exists(paste0("observed_names_",input$gender,".csv"))) {
+            observed_names <- read.csv(paste0("observed_names_",input$gender,".csv"))
+            if(values$nameSuggestion %in% observed_names$name) {
+                
+            } else {
+                new_name <- data.frame(values$nameSuggestion, values$nameCount, values$nameRank)
+                names(new_name) = c("name", "count", "rank")
+                observed_names <- rbind(observed_names, new_name)
+                
+                write.csv(observed_names, paste0("observed_names_",input$gender,".csv"), row.names = FALSE)
+            } 
+                
+        } else {
+            observed_names <- data.frame(values$nameSuggestion, values$nameCount, values$nameRank)
+            names(observed_names) = c("name", "count", "rank")
+            write.csv(observed_names, paste0("observed_names_",input$gender,".csv"), row.names = FALSE)
+        }
+        
+        if (input$gender == 'F') {
+            values$observed <- length(observed_names$name)
+            
+            values$observedCount <- sum(observed_names$count)
+            output$observedNames <- renderText({
+                paste0("Viewed <b>",values$observed,"</b> names, representing <b>",
+                       round(values$observedCount/sum(df[df$gender == input$gender,]$count),digits=2)*100, 
+                       "%</b> of female babies born in 2020")
+            })
+
+        } else {
+            values$observed <- length(observed_names$name)
+            values$observedCount <- sum(observed_names$count)
+            
+            output$observedNames <- renderText({
+                paste0("Viewed <b>",values$observed,"</b> names, representing <b>",
+                       round(values$observedCount/sum(df[df$gender == input$gender,]$count),digits=2)*100, 
+                       "%</b> of male babies born in 2020")
+            })
+
+        }
     })
     
     observeEvent(input$save, {
         if(file.exists(paste0("liked_names_",input$gender,".csv"))) {
             liked_names <- read.csv(paste0("liked_names_",input$gender,".csv"))
             if(values$nameSuggestion %in% liked_names$name) {
-                next
+                
             } else {
                 new_name <- data.frame(values$nameSuggestion, values$nameRank)
                 names(new_name) = c("name", "rank")
                 liked_names <- rbind(liked_names, new_name)
                 write.csv(liked_names, paste0("liked_names_",input$gender,".csv"), row.names = FALSE)
+                if (input$gender == 'F') {
+                    output$likedNames_F <- renderText({
+                        length(liked_names$name)
+                    })
+                } else {
+                    output$likedNames_M <- renderText({
+                        length(liked_names$name) 
+                    })
+                }
             }
         } else {
             new_name <- data.frame(values$nameSuggestion, values$nameRank)
--- baby_name_app/ui.R
+++ baby_name_app/ui.R
@@ -1,37 +1,74 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(bslib)
 
-# Define UI for application that draws a histogram
+# shinyUI(fluidPage(
+# 
+#     titlePanel("Baby Name Suggestion App"),
+# 
+#     sidebarLayout(
+#         sidebarPanel(
+#             checkboxGroupInput("gender",
+#                         h3("Gender"),
+#                         choices = list("Female" = "F",
+#                                        "Male" = "M")),
+#             actionButton("suggest", "Suggest a Name!"),
+#             actionButton("save", "Save this Name")
+#             
+#         ),
+# 
+#         mainPanel(
+#             align="center",
+#             h1(textOutput("nameSuggestion")),
+#             textOutput("nameRank")
+#         )
+#     )
+# ))
+
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Baby Name Suggestion App"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            checkboxGroupInput("gender",
-                        h3("Gender"),
-                        choices = list("Female" = "F",
-                                       "Male" = "M")),
-            actionButton("suggest", "Suggest a Name!"),
-            actionButton("save", "Save this Name")
-            
+    
+    theme = bs_theme(version = 4, bootswatch = "sketchy"),
+    
+    titlePanel(
+        h1("Baby Name Helper", align = "center")
         ),
+    
+    br(),
+    
+    fluidRow(
+        
+        column(3, h4("Gender", align = "right")),
+        column(3, 
+                radioButtons("gender", "",
+                             choices = list("Female" = "F",
+                                            "Male" = "M"))
+               ),
+        column(3, h4("Repeat names?", align = "right")),
+        column(3,
+               radioButtons("rpt", "",
+                            choices = list("Yes" = "yes",
+                                           "No" = "no")))
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            align="center",
-            h1(textOutput("nameSuggestion")),
-            textOutput("nameRank")
+    ),
+    
+    fluidRow(
+        column(6, align = "center",
+            actionButton("suggest", "Suggest a Name!")
+            ),
+        column(6, align = "center",
+            actionButton("save", "Save this Name!")
         )
+    ),
+    
+    br(),
+        
+    verticalLayout(
+        h1(textOutput("nameSuggestion"), align = "center"),
+        h6(textOutput("nameRank"), align = "center"),
+        br(),
+        h6(htmlOutput("observedNames"), align = "center")
+        #h6(textOutput("observedPct"), align = "center")
     )
+
+    
 ))
